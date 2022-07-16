cdaf6aa2db0a2a141d84bcac97b42a5c347ee0aa
berkeskr <berkeskr@gmail.com>
2022-07-16 04:20:14 +0300

    all files

---
+++ src/main/java/com/example/libraryautomationsystem/LibraryAutomationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.libraryautomationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryAutomationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryAutomationSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/libraryautomationsystem/LibraryAutomationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.libraryautomationsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryAutomationSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

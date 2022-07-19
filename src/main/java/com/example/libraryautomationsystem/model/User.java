8d54e9cb91f2407fda05ffb764710e777f092a4e
berkeskr <berkeskr@gmail.com>
2022-07-19 15:04:57 +0300

    User abstract class added

---
+++ src/main/java/com/example/libraryautomationsystem/model/User.java
@@ -0,0 +1,21 @@
+package com.example.libraryautomationsystem.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@MappedSuperclass
+public abstract class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    private String name;
+    private String surname;
+    private String email;
+
+    public void getPersonalInfo(){
+        System.out.println("Name: "+this.name+" Surname: "+this.surname);
+    }
+}
